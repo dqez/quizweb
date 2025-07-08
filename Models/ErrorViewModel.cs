41f70f74f57720fe94ae57f73fac55354abaa3ab
Zeq <124875024+ZEQ-10@users.noreply.github.com>
2025-07-08 22:09:54 +0700

    Initial quizweb project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace quizweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
