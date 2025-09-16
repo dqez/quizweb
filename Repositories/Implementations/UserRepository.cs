8ff8d3a2ee8cf7671a0661bbfbcb2a4ebf8084cf
Zeq <124875024+ZEQ-10@users.noreply.github.com>
2025-09-16 22:54:55 +0700

    Repositories

---
+++ Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using quizweb.Models;
+using quizweb.Repositories.Interfaces;
+
+namespace quizweb.Repositories.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task AddUserAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ApplicationUser>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUser?> GetProfileAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProfile(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using quizweb.Models;
+
+namespace quizweb.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<Ranking>> GetTopRankingsAsync(int topN);
+        Task<IEnumerable<QuestionSet>> GetCreatedQuestionSetsAsync(string username);
+        Task<IEnumerable<ProgressQuestionSet>> GetProgressQuestionSetsAsync(string username);
+        Task<IEnumerable<MarkedQuestion>> GetMarkedQuestionAsync(string username);
+        Task<ApplicationUser> GetProfileAsync(string username);
+        Task UpdateProfileAsync(ApplicationUser user);
+    }
+}
