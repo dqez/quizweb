63672386a0aec20ce8ac5dc5bb807d34f5a9e2e7
dqez <124875024+dqez@users.noreply.github.com>
2025-10-08 23:48:38 +0700

    Quiz/Create: Controller and View

--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using quizweb.Services.Implementaitions;
 using quizweb.Services.Interfaces;
 using quizweb.ViewModels.QuestionSet;
@@ -11,11 +12,15 @@ namespace quizweb.Controllers
     {
         private readonly ILogger<QuizController> _logger;
         private readonly IQuizService _quizService;
+        private readonly ILevelService _levelService;
+        private readonly ICategoryService _categoryService;
 
-        public QuizController(ILogger<QuizController> logger, IQuizService quizService)
+        public QuizController(ILogger<QuizController> logger, IQuizService quizService, ILevelService levelService, ICategoryService categoryService)
         {
             _logger = logger;
             _quizService = quizService;
+            _levelService = levelService;
+            _categoryService = categoryService;
         }
 
         public IActionResult Index()
@@ -23,9 +28,24 @@ namespace quizweb.Controllers
             return View();
         }
 
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            return View();
+            var levelList = await _levelService.GetAllLevelsAsync();
+            var cateList = await _categoryService.GetAllCategoryAsync();
+            var viewModel = new CreateQuestionSetViewModel
+            {
+                Levels = levelList.Select(l => new SelectListItem
+                {
+                    Text = l.LevelName,
+                    Value = l.LevelId.ToString()
+                }).ToList(),
+                Categories = cateList.Select(c => new SelectListItem
+                {
+                    Text = c.CategoryName,
+                    Value = c.CategoryId.ToString()
+                }).ToList()
+            };
+            return View(viewModel);
         }
 
         [HttpPost]
